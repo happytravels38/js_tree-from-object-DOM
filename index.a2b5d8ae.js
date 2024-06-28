89fec0d9bf9f57fdd940a86769f5734659212457
happytravels38 <happytravels38@gmail.com>
2024-06-28 14:23:37 -0400

    make build

---
+++ index.a2b5d8ae.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var l=document.createElement("li");l.textContent=a,"object"==typeof t[a]&&null!==t[a]&&e(l,t[a]),r.appendChild(l)}n.appendChild(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.a2b5d8ae.js.map
